d6ac0d31aa597ab5b3110c9bf7b883a611ee00d9
root <patriciameedina@gmail.com>
2022-03-01 12:15:23 -0900

    no funciona nada de la base de datos

--- srcs/requirements/wordpress/config/initwp.sh
+++ srcs/requirements/wordpress/config/initwp.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-echo $DB_NAME
-echo $DB_USER
-echo $DB_PASSWORD
-
 sed -i "s/db_name/$DB_NAME/" /var/www/html/wordpress/wp-config.php
 sed -i "s/db_user/$DB_USER/" /var/www/html/wordpress/wp-config.php
 sed -i "s/db_password/$DB_PASSWORD/" /var/www/html/wordpress/wp-config.php
+
+cat /var/www/html/wordpress/wp-config.php
 
 php-fpm7
